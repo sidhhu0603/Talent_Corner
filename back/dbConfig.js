16cdd348775917e7fac4aed974551c046b90ecac
sidhhu0603 <siddhantbhoi03@gmail.com>
2024-06-20 18:26:29 +0530

    updated dbconfig and operation

--- back/dbConfig.js
+++ back/dbConfig.js
@@ -1,37 +1,20 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 100, 
-    queueLimit: 0,
+    timeout: process.env.DB_TIMEOUT,
 });
 
-const poolPromise = pool.promise();
-
-async function testConnection() {
-    try {
-        // Perform a simple query to test the connection
-        await poolPromise.query('SELECT 1');
-        console.log('Connected to database.');
-    } catch (err) {
+connection.connect((err) => {
+    if (err) {
         console.error('Database connection failed:', err.stack);
+        return;
     }
-}
+    console.log('Connected to database.');
+});
 
-testConnection();
-
-module.exports = poolPromise;
-
-
-
-
-
-
-
-
-
+module.exports = connection;
